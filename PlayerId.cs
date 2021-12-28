be37c919fdd3ff25fafb801e8cc0780a198678e7
SpaceLobster2311 <alex.williams2311@gmail.com>
2021-12-27 17:26:06 -0800

    Add project files.

---
+++ PlayerId.cs
@@ -0,0 +1,43 @@
+﻿namespace TheSCoPO
+{
+    public class PlayerId
+    {
+        private string fName;
+        private string lName;
+        private int id;
+        public PlayerId(string _first, string _last, int _id)
+        {
+            this.fName = _first;
+            this.lName = _last;
+            this.id = _id;
+        }
+
+
+
+        //Ask Questions to log variables for PlayerID
+        public static void Setup()
+        {
+
+            Console.WriteLine("Enter First Name: ");
+            var first = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name: ");
+            var last = Console.ReadLine();
+
+            Console.WriteLine("Enter 3 digit id#: ");
+            var id = Int32.Parse(Console.ReadLine());
+           
+     
+            var lawId = new PlayerId(first, last, id);
+
+         
+
+            Console.WriteLine(lawId);
+        }
+
+        public override string ToString()
+        {
+            return fName + " " + lName + id;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheSCoPO // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PlayerId.Setup();
+            Console.WriteLine("Program.cs");
+        }
+    }
+}
